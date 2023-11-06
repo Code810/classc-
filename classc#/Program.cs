59943733b6ceca07bbe1ea877deffef83e74ec37
code810 <nadirssh@code.edu.az>
2023-11-06 13:54:31 +0400

    task2 ready

--- classc#/Program.cs
+++ classc#/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using classc_.models;
+using System.Text;
+#region task1
+
+
+//string input = Console.ReadLine();
+//Console.WriteLine(Reverse(input));
+//StringBuilder Reverse(string input)
+//{
+//    StringBuilder result = new();
+//    for (int i = input.Length - 1; i >= 0; i--)
+//    {
+//        result.Append(input[i]);
+//    }
+
+//    return result;
+//}
+#endregion
+#region task2
+Console.Write("Username:");
+string username = Console.ReadLine();
+Console.Write("Password:");
+string password = Console.ReadLine();
+Account input2 = new(username, password);
+input2.Condition();
+#endregion
---
+++ classc#/models/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classc_.models
+{
+    internal class Account
+    {
+        public string UserName;
+        public string Password;
+       
+            public Account(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+           
+        }
+        public void Condition()
+        {
+
+            if (UserName == "Admin123" && Password == "12345a")
+                Console.WriteLine("girisiniz tesdiqlendi");
+            else
+            {
+
+                Console.WriteLine("duzgun daxil etmediniz");
+
+            }
+        }
+
+
+    }
+}
